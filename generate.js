8914b25e3ef339ca31e4dac0266e1790cb32f03b
Sepp Renfer <sepp.renfer@gmail.com>
2017-11-06 15:16:16 +0000

    generate tickdata custom

--- generate.js
+++ generate.js
@@ -1,100 +1,85 @@
+run(new Date(2013,0,1,9,30), 5);
+
+
 
 Array.prototype.flatMap = function(lambda) {
     return Array.prototype.concat.apply([], this.map(lambda));
 };
 
 Date.prototype.addDays = function(days) {
-  var dat = new Date(this.valueOf());
+  let dat = new Date(this.valueOf());
   dat.setDate(dat.getDate() + days);
   return dat;
 }
 
 Date.prototype.addMilliseconds = function(milliseconds) {
-  var dat = new Date(this.valueOf());
+  let dat = new Date(this.valueOf());
   dat.setMilliseconds(dat.getMilliseconds() + milliseconds);
   return dat;
 }
 
-function generateMonthlyChunkFor(year, month) {
-
-    function getRandomArbitrary(min, max) {
-      return Math.random() * (max - min) + min;
-    }
-
-    function getRandomInt(min, max) {
-      min = Math.ceil(min);
-      max = Math.floor(max);
-      return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-    }
-
-    function generateTickFor(ts) {
-        { "askBid": Math.random() > 0.5 ? 'ASK' : 'BID'),
-          "amount": getRandomInt(0,10000),
-          "price": getRandomArbitrary(24,100),
-          "ts": timestamp
-        }
-    }
-
-    function getDaysOfMonth(year, month) {
-      var dateArray = new Array();
-      var startDate = new Date(year, month-1, 1, 9, 30);
-      var endDate = new Date(year, month, 0, 9, 30);
-      while (startDate <= endDate) {
-          dateArray.push(new Date (startDate));
-          startDate = startDate.addDays(1);
-      }
-      return dateArray;
-    }
-
-    function getTickTimneStampsForDay(date) {
-      var timeStampsArray = new Array();
-      var currentDate = new Date(date);
-      while(currentDate.getHours() < 16) {
-        timeStampsArray.push(currentDate);
-        currentDate = currentDate.addMilliseconds(333);
-      }
-      return timeStampsArray;
-    }
-
-    return getDaysOfMonth(year, month).flatMap(day => getTickTimneStampsForDay(day)).map(ts => generateTickFor(ts));
-
-};
-
-
-function generateTickChunkFor(date) {
-  let data = []
-  const openTime = 34200000
-  const closeTime = 57600000
-
-  for(i=openTime;i<closeTime;i+=1000) {
-    data.push(Math.random() > 0.4 ? i : -i); // produce a slight positive trend
-  }
-
-
-  const chunk = {
-     "date": date,
-     "symbol":"MDB",
-     "name": "MongoDB, Inc.",
-     "tickSize": 0.01,
-     "ticks": data
-  }
-
-  return chunk;
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
 }
 
-function generateDates(start, days) {
-  var dates = [];
+function getRandomInt(minp, maxp) {
+  const min = Math.ceil(minp);
+  const max = Math.floor(maxp);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+function generateTickFor(timestamp) {
+    return [ Math.random() > 0.5 ? 'ASK' : 'BID', getRandomInt(0,10000), getRandomArbitrary(24,100), timestamp ];
+}
+
+function getDates(from, days) {
+  let dateArray = new Array();
+  let currentDate = from;
+
   for(i=0;i<days;i++) {
-    dates.push(new Date(start.setDate(start.getDate() + 1)))
+      dateArray.push(new Date (currentDate));
+      currentDate = currentDate.addDays(1);
   }
-  return dates;
+
+  return dateArray;
 }
 
-// db.mdb.drop();
-// generateDates(new Date(2006,0,0,9,30), 3660).map(date => generateTickChunkFor(date))
-// .forEach(chunk => db.mdb.insert(chunk))
-// db.mdb.createIndex({"date": 1});
-//
-// print("imported 10 years of tick data");
-//
-// db.mdb.find({"date": ISODate("2006-01-20T09:30:00Z")}).explain("allPlansExecution")
+function getTickTimeStampsForDay(date) {
+  let timeStampsArray = new Array();
+  let currentDate = new Date(date);
+  while(currentDate.getHours() < 16) {
+    timeStampsArray.push(currentDate);
+    currentDate = currentDate.addMilliseconds(333);
+  }
+  return timeStampsArray;
+}
+
+function run(from, days) {
+  const start = new Date().getTime();
+
+
+  db.mdb.drop();
+  db.mdb.createIndex({"s": 1})
+
+
+
+  const dates = getDates(from, days);
+  print(dates[0] + ", " + dates[dates.length-1]);
+
+
+  dates.forEach(day => {
+      const dayTicks = getTickTimeStampsForDay(day).map(d => generateTickFor(d))
+      const dayChunk = {
+        s: day,
+        sy: 'mdb',
+        d: dayTicks,
+        c: ['askBid', 'amount', 'price', 'timestamp']
+      };
+      db.mdb.insert(dayChunk);
+    }
+  );
+
+  const end = new Date().getTime();
+  const time = end - start;
+  print('Execution time to store ' + days + ' days of tick data: ' + time /1000 + ' sec.');
+};
